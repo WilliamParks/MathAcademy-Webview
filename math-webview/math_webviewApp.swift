f1e8b48035407e25cd520e9f1b0b7b794860d9a5
Bill Parks <c15william.parks@gmail.com>
2025-01-11 17:52:31 -0500

    Initial Commit

---
+++ math-webview/math_webviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  math_webviewApp.swift
+//  math-webview
+//
+//  Created by Bill Parks on 1/11/25.
+//
+
+import SwiftUI
+
+@main
+struct math_webviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ math-webviewTests/math_webviewTests.swift
@@ -0,0 +1,17 @@
+//
+//  math_webviewTests.swift
+//  math-webviewTests
+//
+//  Created by Bill Parks on 1/11/25.
+//
+
+import Testing
+@testable import math_webview
+
+struct math_webviewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
